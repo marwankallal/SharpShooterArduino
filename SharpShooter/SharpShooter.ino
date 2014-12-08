a98709e9865cadd3a54a96858ffbe26112cab7c8
Marwan Kallal <9y7h0n@gmail.com>
2014-12-07 23:57:08 -0500

    write start to servo code

--- SharpShooter/SharpShooter.ino
+++ SharpShooter/SharpShooter.ino
@@ -1,26 +1,34 @@
+#include <Servo.h> 
 
 #define MOTORPIN 9
 #define SONARPIN 1
 #define SERVOPIN 11
 
-//servo positions (0, .5, 1, ||1.5||, 2, 2.5, 3)
+//servo positions (0, 1, 2, ||3||, 4, 5, 6)
 
 //Base voltage for voltage equation (0 point)
-int baseVoltage = 0; //FIXME: FIND THE RIGHT VOLTAGE
+int baseVoltage = 0; //FIXME: FIND THE RIGHT STARTING VOLTAGE
 
 int currentCup = 0;
 
 //user input buffer
 char userIn[1]; 
 
+Servo trackServo;
+
 void setup(){
-  
+  //basic setup
   Serial.begin(9600);
+  trackServo.attach(SERVOPIN);
   
+  //start user promtp and play flow
   Serial.println("Welcome to SharpShooter!\nPlease (s)tart to begin or (c)alibrate");
   
   if(waitAndGetLetter() == 'c'){
     calibrate();
+  }
+  else{
+    moveServoToPosition(4);
   }
 }
 
@@ -31,9 +39,8 @@ void loop(){
 }
 
 void calibrate(){
-  //TODO: BUILD ME!
-  Serial.println("Please enter a new base voltage:");
-  baseVoltage = waitAndGetNumber();
+  Serial.println("Please enter a new base voltage (IN ARDUINO STEPS):");
+  baseVoltage = waitAndGetInt();
 }
 
 char waitAndGetLetter(){
@@ -43,7 +50,7 @@ char waitAndGetLetter(){
   return userIn[0];
 }
 
-int waitAndGetNumber(){
+int waitAndGetInt(){
   //wait for user
   while (Serial.available()==0){ } 
   return Serial.parseInt();
@@ -51,29 +58,39 @@ int waitAndGetNumber(){
 
 int getCupPosition(int cupNum){
   switch(cupNum){
-    case 0: return 1.5;
+    case 0: return 3;
             break;
-    case 1: return 1;
+    case 1: return 2;
             break;
-    case 2: return 2;
+    case 2: return 4;
             break;
-    case 3: return 0.5;
+    case 3: return 1;
             break;
-    case 4: return 1.5;
+    case 4: return 3;
             break;
-    case 5: return 2.5;
+    case 5: return 5;
             break;
     case 6: return 0;
             break;
-    case 7: return 1;
+    case 7: return 2;
             break;
-    case 8: return 2;
+    case 8: return 4;
             break;
-    case 9: return 3;
+    case 9: return 6;
             break;
     default: Serial.println("ERROR: NO SUCH CUP (getCupPosition)");
             break;   
   }
+}
+
+void moveServoToPosition(int pos){
+  //NOTE: CONTINUOUS ROTATION SERVOS HAVE NO ANGLE MODIFIERS. THEY TAKE SPEEDS (180 FULL SPEED, -180 FULL REVERSE)
+  int mov = pos - getCupPosition(currentCup);
+  
+  mov > 0 ? trackServo.write(180) : trackServo.write(0); //check which way to move and move there
+  delay(200 * mov); //TODO: FIND PROPER DELAY FOR ONE POSITION
+  trackServo.write(90);
+  
 }
 
 int getCupRow(int cupNum){
--- Tests/ServoSweep/ServoSweep.ino
+++ Tests/ServoSweep/ServoSweep.ino
@@ -8,19 +8,29 @@ int pos = 0;    // variable to store the servo position
 void setup() 
 { 
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
+  myservo.write(37); 
+  
+  delay(1000);
+  
+  for(int pos = myservo.read(); pos < pos + 180; pos += 1)  // goes from pos degrees to pos + rot degrees 
+    {                                  // in steps of 1 degree 
+      myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+      delay(15);                       // waits 15ms for the servo to reach the position 
+    }
+  
 } 
  
  
 void loop() 
 { 
-  for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
-  {                                  // in steps of 1 degree 
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(15);                       // waits 15ms for the servo to reach the position 
-  } 
-  for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
-  {                                
-    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-    delay(15);                       // waits 15ms for the servo to reach the position 
-  } 
+//  for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
+//  {                                  // in steps of 1 degree 
+//    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+//    delay(15);                       // waits 15ms for the servo to reach the position 
+//  } 
+//  for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
+//  {                                
+//    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+//    delay(15);                       // waits 15ms for the servo to reach the position 
+//  } 
 }
